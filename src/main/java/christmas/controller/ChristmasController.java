17f8bb8c09d1d75e5d94235bf168d8c5b3d09c6a
74059 <shyeone@naver.com>
2023-11-10 12:07:17 +0900

    feat: 식당 예상 방문 날짜 작성 요청

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,9 +1,15 @@
 package christmas.controller;
 
 import christmas.view.OutputView;
+import christmas.view.InputView;
 
 public class ChristmasController {
     public void run () {
         OutputView.printStartEvent();
+        expectedVisitDate();
+    }
+
+    public void expectedVisitDate () {
+        String visitDate =  InputView.readDate();
     }
 }
