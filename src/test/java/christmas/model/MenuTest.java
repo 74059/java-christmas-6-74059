a930a58093331fe0e49538cc8135db40a98b39df
74059 <shyeone@naver.com>
2023-11-13 21:05:12 +0900

    test: drink 메뉴만 pick

--- src/test/java/christmas/model/MenuTest.java
+++ src/test/java/christmas/model/MenuTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MenuTest {
@@ -13,5 +14,16 @@ public class MenuTest {
         List<String> allMenu = Menu.getAllMenu();
         Assertions.assertThat(allMenu).contains("양송이수프", "타파스", "시저샐러드", "티본스테이크",
                 "바비큐립", "해산물파스타", "크리스마스파스타", "초코케이크", "아이스크림", "제로콜라", "레드와인", "샴페인");
+    }
+
+    @DisplayName("특정 menuType(drink)의 메뉴를 가져온다.")
+    @Test
+    void menuGetSpecificMenuType() {
+        List<Menu> drinkMenu = Menu.getMenuOfSpecificMenuType("drink");
+        List<String> drinkMenuForMark = new ArrayList<>();
+        for (Menu eachDrinkMenu : drinkMenu) {
+            drinkMenuForMark.add(eachDrinkMenu.toString());
+        }
+        Assertions.assertThat(drinkMenuForMark).contains("zeroCoke", "redWine", "champagne");
     }
 }
